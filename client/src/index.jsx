af59dbac0345fa6839a9deebb65eb7dcda7e3a40
Thao Le <ttle09@gmail.com>
2020-04-02 18:35:46 -0400

    CRUD with existing database

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App1 from './components/App.jsx';
-
-window.App1 = App1;
+import App from './components/App.jsx';
+const axios = require('axios');
 
 let url = new URL(window.location);
 let params = new URLSearchParams(url.search);
 let id = params.get('id')
-ReactDOM.render(
-  React.createElement(App1, {id} , null),
-  document.getElementById('root')
-)
+
+ReactDOM.render(<App id={id}/>,
+document.getElementById('root'))
+
+
