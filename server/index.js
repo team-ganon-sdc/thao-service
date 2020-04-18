c447fe924c56ab0050f174f42793979d3e98e268
Thao Le <ttle09@gmail.com>
2020-04-18 11:49:44 -0400

    postgres prod

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + '/../client/dist/style.css'));
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
